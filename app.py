8c30bf4865298a2d7c9d4409187ca78efdf05809
sergiopuertas <sergiopuertasp@gmail.com>
2025-03-18 13:24:56 +0100

    fix: refactor and graphs

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import streamlit as st
-from prophet import Prophet
-import altair as alt
-import pandas as pd
+from lib import *
 
-conn = st.connection("neon", type="sql")
 
-# ---------------   CONFIG    ------------------#
-
+# --------------- CONFIGURACIÓN DE LA PÁGINA -----------------#
 st.markdown("""
     <style>
         /* Quitar márgenes y padding del contenedor principal */
@@ -18,189 +13,62 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-category_map = {
-    'STOLEN VEHICLE': 'VEHICLE - STOLEN',
-    'VEHICLE STEALING ATTEMPT': 'VEHICLE - ATTEMPT STOLEN',
-    'OTHER VEHICLES STOLEN': 'VEHICLE, STOLEN - OTHER (MOTORIZED SCOOTERS, BIKES, ETC)',
-    'BURGLARY': 'BURGLARY FROM VEHICLE',
-    'BURGLARY (ATTEMPT)': 'BURGLARY FROM VEHICLE, ATTEMPTED',
-    'THROWING OBJECT AT VEHICLE': 'THROWING OBJECT AT MOVING VEHICLE',
-    'THEFT FROM MOTOR VEHICLE-ATTEMPT': 'THEFT FROM MOTOR VEHICLE - ATTEMPT',
-    'THEFT FROM MOTOR VEHICLE (PETTY)': 'THEFT FROM MOTOR VEHICLE - PETTY ($950 & UNDER)',
-    'THEFT FROM MOTOR VEHICLE (GRAND)': 'THEFT FROM MOTOR VEHICLE - GRAND ($950.01 AND OVER)',
-    'SHOTS FIRED AT VEHICLE': 'SHOTS FIRED AT MOVING VEHICLE, TRAIN OR AIRCRAFT'
-}
+# --------------- SECCIÓN PRINCIPAL -----------------#
 
-freqmap = {
-    "Por mes": ["month", 1, 12, "MS", "%b %Y", [True, False]],
-    "Por semana": ["week", 1, 52, "W", "%d %b %Y", [True, True]],
-    "Por trimestre": ["quarter", 1, 8, "QS", None, [True, False]]
-}
-
-
-def format_quarter(date):
-    quarter = (date.month - 1) // 3 + 1
-    return f"T{quarter} {date.year}"
-
-
-# ---------------   PAGE    ------------------#
-
-places_q = f"""
-    SELECT DISTINCT areaname
-    FROM main
-"""
-
-places = conn.query(places_q,ttl = "10m")['areaname']
+# Obtener lugares únicos
+places = get_unique_places()
 
 st.title('Analizador de crimen sobre vehículos')
+# Filtros
 
-with st.popover("Filtros"):
-    col = st.columns(2)
-    with col[0]:
-        predict = st.checkbox("Predicción de crimen a futuro", value=False)
-    with col[1]:
-        pond = st.checkbox("Ponderar crímenes", value=False)
+col1, col2,_,col3= st.columns((1,1,2,1))
+with col1:
+    predict = st.checkbox("Predicción de crimen a futuro", value=False)
+with col2:
+    pond = st.checkbox("Ponderar crímenes", value=False)
+with col3:
+    with st.popover("Segmentación"):
+        chosen_crime = st.multiselect('Segmentado por crimen',
+                                      sorted(category_map.keys()),
+                                      default=None)
+        chosen_place = st.multiselect('Segmentado por lugar',
+                                      sorted(places),
+                                      default=None)
+freq_choice = st.radio("Predicción de crimen a futuro",
+                           ["Por trimestre", "Por mes", "Por semana"],
+                           label_visibility="collapsed",
+                           horizontal=True)
+freq = freqmap[freq_choice]
+# Construir condiciones de filtrado
+crime_conditions, place_conditions = build_conditions(chosen_crime, chosen_place)
 
-    freq = freqmap[st.radio("Predicción de crimen a futuro", ["Por trimestre", "Por mes", "Por semana"],
-                            label_visibility="collapsed", horizontal=True)]
-
-    chosen_crime = st.multiselect('Segmentado por crimen', sorted(category_map.keys(), key=lambda x:
-    x), label_visibility="visible", default=None)
-    chosen_place = st.multiselect('Segmentado por lugar', sorted(places),
-                                  label_visibility="visible", default=None)
-
-# Filtrar con base en los filtros elegidos
-crime_conditions = " OR ".join(
-    [f"crimecodedesc = '{category_map[crime]}'" for crime in chosen_crime]) if chosen_crime else "1=1"
-place_conditions = " OR ".join([f"areaname = '{place}'" for place in chosen_place]) if chosen_place else "1=1"
-
-# Construir la consulta SQL
+# Consulta general
 sql_query = f"""
     SELECT date, crimecodedesc, areaname, pond
     FROM main
     WHERE ({crime_conditions})
-    AND ({place_conditions})
+      AND ({place_conditions})
 """
+data = conn.query(sql_query, ttl="10m")
 
-data = conn.query(sql_query,ttl = "10m")
-
+# Consulta agrupada para el gráfico
+grouped = fetch_grouped_data(crime_conditions, place_conditions, freq)
+grouped = apply_ponderation_to_data(grouped, pond)
+# --------------- GRÁFICOS -----------------#
 if predict:
     st.header("Predicción de Crimen a Futuro")
-    n_steps = st.slider("Número de etapas a predecir", min_value=freq[1], max_value=freq[2],
-                        value=(freq[1] + freq[2]) // 2, label_visibility="collapsed")
+    n_steps = st.slider("Número de etapas a predecir",
+                        min_value=freq[1], max_value=freq[2],
+                        value=(freq[1] + freq[2]) // 2,
+                        label_visibility="collapsed")
+
+    forecast = forecast_data(grouped, freq, n_steps)
+    chart = create_combined_chart(grouped, forecast)
 else:
-    n_steps = 0
-
-# ---------------   DATA    ------------------#
-
-grouped_query = f"""
-    SELECT
-        DATE_TRUNC('{freq[0]}', date) AS period,
-        COUNT(*) AS count,
-        AVG(pond) AS pond
-    FROM main
-    WHERE ({crime_conditions})
-    AND ({place_conditions})
-    GROUP BY period
-    ORDER BY period
-"""
-
-grouped = conn.query(grouped_query, ttl = "10m")
-
-print("Fecha mínima en los datos:", grouped['period'].min())
-print("Fecha máxima en los datos:", grouped['period'].max())
-
-# Aplicar ponderación si se ha seleccionado
-if pond:
-    total_original = grouped['count'].sum()
-    total_ponderado = (grouped['count'] * grouped['pond']).sum()
-    normalization_factor = total_original / total_ponderado
-    grouped['count'] = grouped['count'] * grouped['pond'] * normalization_factor
-
-# ---------------   CHART    ------------------#
-
-if predict:
-    # Preparar los datos para Prophet
-    prophet_data = grouped[['period', 'count']].rename(columns={'period': 'ds', 'count': 'y'})
-    prophet_data['ds'] = pd.to_datetime(prophet_data['ds'], utc = True).dt.tz_localize(None)
-
-    # Ajustar el modelo Prophet
-    model_prophet = Prophet(yearly_seasonality=freq[5][0], weekly_seasonality=freq[5][1], daily_seasonality=False)
-    model_prophet.fit(prophet_data)
-
-    # Crear fechas futuras
-    future = model_prophet.make_future_dataframe(periods=n_steps, freq=freq[3])
-    forecast = model_prophet.predict(future)
-
-    # Preparar los datos para el gráfico
-    last_date = prophet_data['ds'].max()
-    forecast['tipo'] = forecast['ds'].apply(lambda x: 'Histórico' if x <= last_date else 'Predicción')
-    data_to_plot = forecast[['ds', 'yhat', 'yhat_upper', 'yhat_lower', 'tipo']]
-
-
-
-    y_min = forecast['yhat'].min()
-    y_max = forecast['yhat'].max()
-    if y_min < 0:
-        y_min = 0
-    rango = y_max - y_min
-    if rango < 1e-3:
-        domain = [y_min - 1, y_max + 1]
-    else:
-        padding = rango * 0.1
-        domain = [y_min - padding, y_max + padding]
-
-    scale_y = alt.Scale(domain=domain, nice=True)
-
-    line_chart = alt.Chart(data_to_plot).mark_line(point=True).encode(
-        x=alt.X('ds:T', title='Fecha'),
-        y=alt.Y('yhat:Q', title='Valor', scale=scale_y),
-        color=alt.Color(
-            'tipo:N',
-            scale=alt.Scale(domain=['Histórico', 'Predicción'], range=['lightblue', 'red']),
-            legend=None
-        )
-    ).properties(
-        width=700,
-        height=600,
-    )
-
-    band_chart = alt.Chart(data_to_plot[data_to_plot["tipo"] == "Predicción"]).mark_area(opacity=0.3).encode(
-        x=alt.X('ds:T', title='Fecha'),
-        y=alt.Y('yhat_lower:Q', title='Valor', scale=scale_y),
-        y2="yhat_upper:Q"
-    ).properties(
-        width=700,
-        height=600,
-    )
-    chart = band_chart + line_chart
-else:
-    chart = alt.Chart(grouped).mark_line(point=True).encode(
-        x=alt.X('period:T', title='Fecha'),
-        y=alt.Y('count:Q', title='Valor',
-                scale=alt.Scale(domain=[grouped['count'].min(), grouped['count'].max()])),
-        color=alt.value('lightblue')
-    ).properties(
-        width=700,
-        height=600,
-    )
+    chart = create_historical_chart(grouped)
 
 st.altair_chart(chart, use_container_width=True)
 
-# ---------------   KPI    ------------------#
-max_date = grouped.loc[grouped['count'].idxmax(), 'period']
-min_date = grouped.loc[grouped['count'].idxmin(), 'period']
-
+# --------------- KPIs -----------------#
 if not predict:
-    with st.container():
-        columns = st.columns(4)
-        with columns[0]:
-            st.metric('Total', int(grouped['count'].sum()))
-        with columns[1]:
-            st.metric('Media', int(grouped['count'].mean()))
-        with columns[2]:
-            max_period = format_quarter(max_date) if freq[0] == 'quarter' else max_date.strftime(freq[4])
-            st.metric('Periodo con más crímenes', max_period)
-        with columns[3]:
-            min_period = format_quarter(min_date) if freq[0] == 'quarter' else min_date.strftime(freq[4])
-            st.metric("Periodo con menos crímenes", min_period)
+    display_kpis(grouped, freq)
---
+++ lib.py
@@ -0,0 +1,161 @@
+import streamlit as st
+from prophet import Prophet
+import altair as alt
+import pandas as pd
+
+# Mapas de categorías y configuraciones de frecuencia
+category_map = {
+    'STOLEN VEHICLE': 'VEHICLE - STOLEN',
+    'VEHICLE STEALING ATTEMPT': 'VEHICLE - ATTEMPT STOLEN',
+    'OTHER VEHICLES STOLEN': 'VEHICLE, STOLEN - OTHER (MOTORIZED SCOOTERS, BIKES, ETC)',
+    'BURGLARY': 'BURGLARY FROM VEHICLE',
+    'BURGLARY (ATTEMPT)': 'BURGLARY FROM VEHICLE, ATTEMPTED',
+    'THROWING OBJECT AT VEHICLE': 'THROWING OBJECT AT MOVING VEHICLE',
+    'THEFT FROM MOTOR VEHICLE-ATTEMPT': 'THEFT FROM MOTOR VEHICLE - ATTEMPT',
+    'THEFT FROM MOTOR VEHICLE (PETTY)': 'THEFT FROM MOTOR VEHICLE - PETTY ($950 & UNDER)',
+    'THEFT FROM MOTOR VEHICLE (GRAND)': 'THEFT FROM MOTOR VEHICLE - GRAND ($950.01 AND OVER)',
+    'SHOTS FIRED AT VEHICLE': 'SHOTS FIRED AT MOVING VEHICLE, TRAIN OR AIRCRAFT'
+}
+
+freqmap = {
+    "Por mes": ["month", 1, 12, "MS", "%b %Y", [True, False]],
+    "Por semana": ["week", 1, 52, "W", "%d %b %Y", [True, True]],
+    "Por trimestre": ["quarter", 1, 8, "QS", None, [True, False]]
+}
+
+# Conexión a la base de datos
+conn = st.connection("neon", type="sql")
+
+# --------------- FUNCIONES AUXILIARES -----------------#
+
+
+def format_quarter(date):
+    quarter = (date.month - 1) // 3 + 1
+    return f"T{quarter} {date.year}"
+
+def get_unique_places():
+    query = "SELECT DISTINCT areaname FROM main"
+    result = conn.query(query, ttl="10m")
+    return result['areaname']
+
+
+def build_conditions(chosen_crime, chosen_place):
+    crime_conditions = " OR ".join(
+        [f"crimecodedesc = '{category_map[crime]}'" for crime in chosen_crime]
+    ) if chosen_crime else "1=1"
+
+    place_conditions = " OR ".join(
+        [f"areaname = '{place}'" for place in chosen_place]
+    ) if chosen_place else "1=1"
+
+    return crime_conditions, place_conditions
+
+
+def fetch_grouped_data(crime_conditions, place_conditions, freq):
+    query = f"""
+        SELECT
+            DATE_TRUNC('{freq[0]}', date) AS period,
+            COUNT(*) AS count,
+            AVG(pond) AS pond
+        FROM main
+        WHERE ({crime_conditions})
+          AND ({place_conditions})
+        GROUP BY period
+        ORDER BY period
+    """
+    return conn.query(query, ttl="10m")
+
+
+def apply_ponderation_to_data(grouped, apply_ponder):
+    if apply_ponder:
+        total_original = grouped['count'].sum()
+        total_ponderado = (grouped['count'] * grouped['pond']).sum()
+        normalization_factor = total_original / total_ponderado
+        grouped['count'] = grouped['count'] * grouped['pond'] * normalization_factor
+    return grouped
+
+
+def forecast_data(grouped, freq, n_steps):
+    prophet_data = grouped[['period', 'count']].rename(columns={'period': 'ds', 'count': 'y'})
+    prophet_data['ds'] = pd.to_datetime(prophet_data['ds'], utc=True).dt.tz_localize(None)
+
+    model = Prophet(yearly_seasonality=freq[5][0],
+                    weekly_seasonality=freq[5][1],
+                    daily_seasonality=False)
+    model.fit(prophet_data)
+
+    future = model.make_future_dataframe(periods=n_steps, freq=freq[3])
+    forecast = model.predict(future)
+    last_date = prophet_data['ds'].max()
+    forecast['tipo'] = forecast['ds'].apply(lambda x: 'Histórico' if x <= last_date else 'Predicción')
+
+    return forecast
+
+
+def create_combined_chart(grouped, forecast):
+    # Concatenar los datos históricos y las predicciones
+    historical_data = grouped[['period', 'count']].rename(columns={'period': 'ds', 'count': 'yhat'})
+    historical_data['ds'] = pd.to_datetime(historical_data['ds'], utc=True).dt.tz_localize(None)
+    historical_data['tipo'] = 'Histórico'
+    # Datos de predicción
+
+    forecast_data = forecast[['ds', 'yhat', 'yhat_upper', 'yhat_lower', 'tipo']]
+    last_historical_date = historical_data['ds'].max()
+    forecast_data_filtered = forecast_data[forecast_data['ds'] > last_historical_date]
+
+    combined_data = pd.concat([historical_data, forecast_data_filtered], ignore_index=True)
+
+    # Definir el rango de valores en el eje Y
+    y_min = combined_data['yhat'].min()
+    y_max = combined_data['yhat'].max()
+    if y_min < 0:
+        y_min = 0
+    range_diff = y_max - y_min
+    padding = range_diff * 0.1 if range_diff > 1e-3 else 1
+    domain = [y_min - padding, y_max + padding]
+
+    scale_y = alt.Scale(domain=domain, nice=True)
+
+    line_chart = alt.Chart(combined_data).mark_line(point=True).encode(
+        x=alt.X('ds:T', title='Fecha'),
+        y=alt.Y('yhat:Q', title='Valor', scale=scale_y),
+        color=alt.Color('tipo:N', scale=alt.Scale(domain=['Histórico', 'Predicción'],
+                                                  range=['lightblue', 'red']),
+                        legend=None)
+    ).properties(width=700, height=500)
+
+    band_chart = alt.Chart(combined_data[combined_data["tipo"] == "Predicción"]).mark_area(opacity=0.3).encode(
+        x=alt.X('ds:T', title='Fecha'),
+        y=alt.Y('yhat_lower:Q', title='Valor', scale=scale_y),
+        y2="yhat_upper:Q"
+    ).properties(width=700, height=500)
+
+    return band_chart + line_chart
+
+
+def create_historical_chart(grouped):
+    # Definir dominio para el eje Y
+    return alt.Chart(grouped).mark_line(point=True).encode(
+        x=alt.X('period:T', title='Fecha'),
+        y=alt.Y('count:Q', title='Valor',
+                scale=alt.Scale(domain=[grouped['count'].min(), grouped['count'].max()])),
+        color=alt.value('lightblue')
+    ).properties(width=700, height=500)
+
+
+def display_kpis(grouped, freq):
+    max_date = grouped.loc[grouped['count'].idxmax(), 'period']
+    min_date = grouped.loc[grouped['count'].idxmin(), 'period']
+    max_period = format_quarter(max_date) if freq[0] == 'quarter' else max_date.strftime(freq[4])
+    min_period = format_quarter(min_date) if freq[0] == 'quarter' else min_date.strftime(freq[4])
+
+    with st.container():
+        cols = st.columns(4)
+        with cols[0]:
+            st.metric('Total', int(grouped['count'].sum()))
+        with cols[1]:
+            st.metric('Media', int(grouped['count'].mean()))
+        with cols[2]:
+            st.metric('Periodo con más crímenes', max_period)
+        with cols[3]:
+            st.metric('Periodo con menos crímenes', min_period)
